6773bcbe5cb15df437a74ba09cd4b17478b6f470
Omganesh G <omg20074132m@gmail.com>
2026-09-02 21:43:11 +0530

    Force compact radar as final student dashboard override

--- vite.vercel.config.js
+++ vite.vercel.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import baseConfig from "./vite.config.js";
+import { compactCareerRadarPlugin } from "./src/compactCareerRadar.js";
 import { studentUiPlugin } from "./src/studentUiOverrides.js";
 import { studentDataPlugin } from "./src/studentDataOverrides.js";
 
+const basePlugins = baseConfig.plugins || [];
+const safeBasePlugins = basePlugins.filter((plugin) => !["skillproof-compact-career-radar", "skillproof-student-ui", "skillproof-student-data"].includes(plugin?.name));
+
 export default defineConfig({
   ...baseConfig,
-  plugins: [studentUiPlugin(), studentDataPlugin(), ...(baseConfig.plugins || [])],
+  plugins: [
+    studentUiPlugin(),
+    studentDataPlugin(),
+    compactCareerRadarPlugin(),
+    ...safeBasePlugins,
+  ],
 });
