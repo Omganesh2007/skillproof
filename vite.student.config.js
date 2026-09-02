68f12f46a36fc34514a8d92c0e28137c995b30e0
Omganesh G <omg20074132m@gmail.com>
2026-09-02 21:13:04 +0530

    Wire student profile data synchronization

--- vite.student.config.js
+++ vite.student.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import baseConfig from "./vite.config.js";
 import { radarUiPlugin } from "./src/radarUiOverrides.js";
+import { studentDataPlugin } from "./src/studentDataOverrides.js";
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [radarUiPlugin(), ...(baseConfig.plugins || [])],
+  plugins: [radarUiPlugin(), studentDataPlugin(), ...(baseConfig.plugins || [])],
 });
