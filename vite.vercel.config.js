1a45185d3575c8a590cd35f20bbdfb6ea5219dcd
Omganesh G <omg20074132m@gmail.com>
2026-09-04 13:46:33 +0530

    Use safe JSX plugins for Vercel build

--- vite.vercel.config.js
+++ vite.vercel.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { careerRequirementsPlugin } from "./src/careerRequirementsPlugin.js";
-import { studentOpportunitiesPlugin } from "./src/studentOpportunitiesPlugin.js";
-import { studentWelcomePlugin } from "./src/studentWelcomePlugin.js";
 import { registrationFormPlugin } from "./src/registrationFormPlugin.js";
-import { studentUiPlugin } from "./src/studentUiOverrides.js";
-import { studentDataPlugin } from "./src/studentDataOverrides.js";
+import { collegeIndustryPlugin } from "./src/collegeIndustryPlugin.js";
 
 export default defineConfig({
   plugins: [
     registrationFormPlugin(),
-    studentWelcomePlugin(),
-    studentOpportunitiesPlugin(),
-    careerRequirementsPlugin(),
-    studentUiPlugin(),
-    studentDataPlugin(),
+    collegeIndustryPlugin(),
     react(),
     tailwindcss(),
   ],
