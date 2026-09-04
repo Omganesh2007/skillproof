1481bdb93a7a7a1dbd0e22fea722206bbc2ddcc6
Omganesh G <omg20074132m@gmail.com>
2026-09-04 10:23:29 +0530

    Use expanded career requirements in Vercel build

--- vite.vercel.config.js
+++ vite.vercel.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import baseConfig from "./vite.config.js";
+import { careerRequirementsPlugin } from "./src/careerRequirementsPlugin.js";
 import { careerSkillTreePlugin } from "./src/careerSkillTreePlugin.js";
 import { careerPathsPagePlugin } from "./src/careerPathsPagePlugin.js";
 import { studentUiPlugin } from "./src/studentUiOverrides.js";
@@ -7,6 +8,7 @@ import { studentDataPlugin } from "./src/studentDataOverrides.js";
 
 const basePlugins = baseConfig.plugins || [];
 const excluded = new Set([
+  "skillproof-expanded-career-requirements",
   "skillproof-career-skill-tree",
   "skillproof-career-paths-page",
   "skillproof-compact-career-radar",
@@ -19,6 +21,7 @@ const safeBasePlugins = basePlugins.filter((plugin) => !excluded.has(plugin?.nam
 export default defineConfig({
   ...baseConfig,
   plugins: [
+    careerRequirementsPlugin(),
     studentUiPlugin(),
     studentDataPlugin(),
     careerSkillTreePlugin(),
