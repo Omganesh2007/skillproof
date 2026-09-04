39c5342288516b4f9ab5f5c380a9a27ecf5f24aa
Omganesh G <omg20074132m@gmail.com>
2026-09-04 13:38:33 +0530

    Fix Vercel build by removing OXC JSX transforms

--- vite.vercel.config.js
+++ vite.vercel.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { careerRequirementsPlugin } from "./src/careerRequirementsPlugin.js";
-import { careerSkillTreePlugin } from "./src/careerSkillTreePlugin.js";
-import { completeCareerDashboardPlugin } from "./src/completeCareerDashboardPlugin.js";
-import { careerPathsPagePlugin } from "./src/careerPathsPagePlugin.js";
 import { studentOpportunitiesPlugin } from "./src/studentOpportunitiesPlugin.js";
 import { studentWelcomePlugin } from "./src/studentWelcomePlugin.js";
 import { registrationFormPlugin } from "./src/registrationFormPlugin.js";
@@ -19,9 +16,6 @@ export default defineConfig({
     careerRequirementsPlugin(),
     studentUiPlugin(),
     studentDataPlugin(),
-    careerSkillTreePlugin(),
-    completeCareerDashboardPlugin(),
-    careerPathsPagePlugin(),
     react(),
     tailwindcss(),
   ],
