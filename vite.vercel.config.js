8893e39815b4b09bcb20ce7c4502861fa746eb6e
Omganesh G <omg20074132m@gmail.com>
2026-09-05 21:31:09 +0530

    Use stable single dashboard replacement

--- vite.vercel.config.js
+++ vite.vercel.config.js
@@ -4,7 +4,7 @@ import { collegeDashboardEnhancementPlugin } from "./src/collegeDashboardEnhance
 import { registrationFormPlugin } from "./src/registrationFormPlugin.js";
 import { industryAuthPlugin } from "./src/industryAuthPlugin.js";
 import { industryDashboardPlugin } from "./src/industryDashboardPlugin.js";
-import { dashboardReferencePlugin } from "./src/dashboardReferencePlugin.js";
+import { studentDashboardRedesignPlugin } from "./src/studentDashboardRedesignPlugin.js";
 
 const basePlugins = baseConfig.plugins || [];
 const excluded = new Set([
@@ -25,7 +25,7 @@ const safeBasePlugins = basePlugins.filter((plugin) => !excluded.has(plugin?.nam
 export default defineConfig({
   ...baseConfig,
   plugins: [
-    dashboardReferencePlugin(),
+    studentDashboardRedesignPlugin(),
     collegeDashboardEnhancementPlugin(),
     registrationFormPlugin(),
     industryAuthPlugin(),
